27bfe48bfbda8a17cced4b3e528045d71a34661b
tharik-10 <md.tharik@mygurukulam.co>
2025-05-30 16:43:48 +0530

    Update and rename commonStages.groovy to pipelineSteps.groovy

---
+++ vars/pipelineSteps.groovy
@@ -0,0 +1,11 @@
+def cloneRepository(String repoUrl, String branch = 'main') {
+    echo '📥 Cloning repository...'
+    deleteDir()
+    git url: repoUrl, branch: branch
+}
+
+def buildProject() {
+    echo '🔧 Building the project (skipping tests)...'
+    sh 'mvn clean install -DskipTests'
+}
+
