8e788f9ce59f311219861ae7952e6f78c704654b
tharik-10 <md.tharik@mygurukulam.co>
2025-07-04 11:52:21 +0530

    Create TerraformUtils.groovy

---
+++ src/org/snaatak/TerraformUtils.groovy
@@ -0,0 +1,27 @@
+package org.snaatak
+
+class TerraformUtils implements Serializable {
+  def steps
+
+  TerraformUtils(steps) {
+    this.steps = steps
+  }
+
+  def terraformInit(Map config) {
+    def dir = config.directory
+    def backendConfig = config.backendConfig?.collect { k, v -> "-backend-config=${k}=${v}" }?.join(' ') ?: ''
+    steps.sh "cd ${dir} && terraform init ${backendConfig}"
+  }
+
+  def terraformValidate(Map config) {
+    def dir = config.directory
+    steps.sh "cd ${dir} && terraform validate"
+  }
+
+  def terraformPlan(Map config) {
+    def dir = config.directory
+    def varsArgs = config.vars?.collect { k, v -> "-var '${k}=${v}'" }?.join(' ') ?: ''
+    def outFileArg = config.outFile ? "-out=${config.outFile}" : ''
+    steps.sh "cd ${dir} && terraform plan ${varsArgs} ${outFileArg}"
+  }
+}
