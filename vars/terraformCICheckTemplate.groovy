dea3ac1a4c6f95c913bbb224a4a5e22c01633b12
tharik-10 <md.tharik@mygurukulam.co>
2025-07-05 11:41:07 +0530

    Create terraformCICheckTemplate.groovy

---
+++ vars/terraformCICheckTemplate.groovy
@@ -0,0 +1,41 @@
+def call(Map config = [:]) {
+  def steps = this
+
+  def MODULE_DIR    = config.get('tfModuleDir', '')
+  def BACKEND_CONF  = config.get('backendConfig', [:])
+  def TF_VARS       = config.get('tfVars', [:])
+  def PLAN_OUT_FILE = config.get('planOutFile', 'tfplan.out')
+
+  def tf = new org.terraformci.TerraformUtils(steps)
+
+  node {
+    try {
+      stage('Terraform Init') {
+        tf.terraformInit(
+          directory: MODULE_DIR,
+          backendConfig: BACKEND_CONF
+        )
+      }
+
+      stage('Terraform Validate') {
+        tf.terraformValidate(
+          directory: MODULE_DIR
+        )
+      }
+
+      stage('Terraform Plan') {
+        tf.terraformPlan(
+          directory: MODULE_DIR,
+          vars: TF_VARS,
+          outFile: PLAN_OUT_FILE
+        )
+      }
+
+      currentBuild.result = 'SUCCESS'
+
+    } catch (err) {
+      currentBuild.result = 'FAILURE'
+      throw err
+    }
+  }
+}
