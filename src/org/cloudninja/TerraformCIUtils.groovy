d67a51bf18690f0b787ac2e8f5274335148d1d7e
tharik-10 <md.tharik@mygurukulam.co>
2025-07-10 21:57:17 +0530

    Update TerraformCIUtils.groovy

--- src/org/cloudninja/TerraformCIUtils.groovy
+++ src/org/cloudninja/TerraformCIUtils.groovy
@@ -24,4 +24,16 @@ class TerraformCIUtils implements Serializable {
     def outFileArg = config.outFile ? "-out=${config.outFile}" : ''
     steps.sh "cd ${dir} && terraform plan ${varsArgs} ${outFileArg}"
   }
+
+  def runCheckov(Map config) {
+    def dir = config.directory
+    steps.sh "cd ${dir} && checkov -d . -o json > checkov-report.json || true"
+    steps.archiveArtifacts artifacts: "${dir}/checkov-report.json", fingerprint: true
+  }
+
+  def runTFLint(Map config) {
+    def dir = config.directory
+    steps.sh "cd ${dir} && tflint --init && tflint > tflint.log || true"
+    steps.archiveArtifacts artifacts: "${dir}/tflint.log", fingerprint: true
+  }
 }
