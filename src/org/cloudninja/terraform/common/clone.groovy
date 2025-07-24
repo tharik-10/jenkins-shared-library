8ad95430916e14538df9e4154a584dbe2c5b6200
tharik-10 <md.tharik@mygurukulam.co>
2025-07-24 11:53:39 +0530

    Update clone.groovy

--- src/org/cloudninja/terraform/common/clone.groovy
+++ src/org/cloudninja/terraform/common/clone.groovy
@@ -1,14 +1,14 @@
 package org.cloudninja.terraform.common
 
-def clone(String branch, String repoUrl, String credentialsId = null) {
-    stage('cloning repositories') {
-        echo "Cloning repository from ${repoUrl}, branch: ${branch}"
-
-        if (credentialsId) {
-            echo "Using credentials ID: ${credentialsId}"
-            git branch: branch, url: repoUrl, credentialsId: credentialsId
-        } else {
-            git branch: branch, url: repoUrl
+class gitclone {
+    def clone(String branch, String repoUrl, String credentialsId = null) {
+        stage('Cloning Repository') {
+            echo "Cloning ${repoUrl} branch: ${branch}"
+            if (credentialsId) {
+                git branch: branch, url: repoUrl, credentialsId: credentialsId
+            } else {
+                git branch: branch, url: repoUrl
+            }
         }
     }
 }
