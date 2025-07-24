db27b039ce440a77b48e44ae43958a35eab0a8c2
tharik-10 <md.tharik@mygurukulam.co>
2025-07-24 11:26:46 +0530

    Create checkov.groovy

---
+++ src/org/cloudninja/terraform/ci/checkov.groovy
@@ -0,0 +1,20 @@
+package org.cloudninja.terraform.ci
+
+def terraformcheckov(String terraformDir) {
+    stage('Checkov') {
+    dir(terraformDir) {
+        sh '''
+            if ! command -v checkov &> /dev/null; then
+                pip install --user -q --upgrade checkov
+            fi
+            export PATH=$HOME/.local/bin:$PATH
+        
+            checkov -d . --output cli | tee checkov_report.txt
+
+            PASSED_COUNT=$(grep -oP 'Passed checks: \\K\\d+' checkov_report.txt)
+            echo " Total Passed Checks: $PASSED_COUNT"
+        '''
+        archiveArtifacts artifacts: 'checkov_report.txt', fingerprint: true
+    }
+}
+}
