5da2187c6f12aa28c7654d58a02090385f755b47
tharik-10 <md.tharik@mygurukulam.co>
2025-07-24 11:32:07 +0530

    Create terraform_destroy.groovy

---
+++ src/org/cloudninja/terraform/cd/terraform_destroy.groovy
@@ -0,0 +1,16 @@
+package org.cloudninja.terraform.cd
+
+def call(String terraform_path, String tfvarsFile = "", String exactStatePath = "") {
+    stage('Terraform Destroy') {
+        echo "Destroying Terraform configuration in ${exactStatePath}..."
+
+        sh """
+            echo "Switching to directory: ${exactStatePath}"
+            cd "${exactStatePath}"
+
+            terraform init -input=false
+
+            terraform destroy -auto-approve -var-file=${tfvarsFile}
+        """
+    }
+}
