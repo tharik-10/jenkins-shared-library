bf4eaadae61f64d2bc8123783e279a4028eeeeff
tharik-10 <md.tharik@mygurukulam.co>
2025-07-24 12:44:08 +0530

    Update wsclean.groovy

--- src/org/cloudninja/terraform/common/wsclean.groovy
+++ src/org/cloudninja/terraform/common/wsclean.groovy
@@ -1,13 +1,7 @@
 package org.cloudninja.terraform.common
 
-class wsclean implements Serializable {
-    def steps
-
-    wsclean(steps) {
-        this.steps = steps
-    }
-
-    def clean() {
-        steps.sh 'rm -rf * .terraform*'
+def clean() {
+    stage('Clean Workspace') {
+        cleanWs()
     }
 }
