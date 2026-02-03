524a828932065ba5bca6494f9448d1ac8997f407
tharik-10 <md.tharik@mygurukulam.co>
2026-02-04 04:59:11 +0530

    Update SecurityScanner.groovy

--- src/scanners/SecurityScanner.groovy
+++ src/scanners/SecurityScanner.groovy
@@ -1,9 +1,9 @@
 package scanners
 
 class SecurityScanner {
-    static void run() {
-        sh '''
-        dependency-check.sh --scan . --failOnCVSS 7
-        '''
+    static void run(def steps) {
+        steps.echo "Running Dependency Security Scan..."
+        // Adjust this path to where your dependency-check tool is installed
+        steps.sh 'dependency-check.sh --scan . --failOnCVSS 7'
     }
 }
