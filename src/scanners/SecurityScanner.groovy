02430f48effc53d6825f193be5dfbd9d466781d5
tharik-10 <md.tharik@mygurukulam.co>
2026-02-03 14:30:12 +0530

    Update SecurityScanner.groovy

--- src/scanners/SecurityScanner.groovy
+++ src/scanners/SecurityScanner.groovy
@@ -3,7 +3,6 @@ package scanners
 class SecurityScanner {
     static void run() {
         sh '''
-        mvn sonar:sonar || true
         dependency-check.sh --scan . --failOnCVSS 7
         '''
     }
