978db07fbf97839e743e41d322b3040d047a52a6
tharik-10 <md.tharik@mygurukulam.co>
2026-02-03 14:30:28 +0530

    Update ImageScanner.groovy

--- src/scanners/ImageScanner.groovy
+++ src/scanners/ImageScanner.groovy
@@ -3,7 +3,9 @@ package scanners
 class ImageScanner {
     static void scan(String app, String tag) {
         sh """
-        trivy image ${ECR_URL}/${app}:${tag} --exit-code 1 --severity HIGH,CRITICAL
+        trivy image ${ECR_URL}/${app}:${tag} \
+        --severity HIGH,CRITICAL \
+        --exit-code 1
         """
     }
 }
