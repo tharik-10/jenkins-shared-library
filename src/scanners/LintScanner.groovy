87c734750243c5f19bc2681e2cc164ff3f7cfa1a
tharik-10 <md.tharik@mygurukulam.co>
2026-02-03 18:21:05 +0530

    Update LintScanner.groovy

--- src/scanners/LintScanner.groovy
+++ src/scanners/LintScanner.groovy
@@ -5,22 +5,16 @@ class LintScanner {
         switch(lang) {
             case 'python':
                 steps.sh '''
-                python3 --version
-                python3 -m ensurepip || true
-                python3 -m pip install --upgrade pip
-                python3 -m pip install flake8
-                python3 -m flake8 .
+                pip install flake8
+                flake8 .
                 '''
                 break
-
             case 'go':
                 steps.sh 'golangci-lint run'
                 break
-
             case 'java':
                 steps.sh 'mvn checkstyle:check'
                 break
-
             case 'node':
                 steps.sh '''
                 npm ci
