79c1fcdc550490783b5e0b58a733e6b578a9a736
tharik-10 <md.tharik@mygurukulam.co>
2026-02-04 05:03:58 +0530

    Update TestRunner.groovy

--- src/scanners/TestRunner.groovy
+++ src/scanners/TestRunner.groovy
@@ -4,10 +4,22 @@ class TestRunner {
     static void run(def steps, String lang) {
         steps.echo "Running Unit Tests for ${lang}..."
         switch(lang) {
-            case 'python': steps.sh 'python3 -m pytest'; break
-            case 'go':     steps.sh 'go test ./...'; break
-            case 'java':   steps.sh 'mvn test'; break
-            case 'node':   steps.sh 'npm test'; break
+            case 'python':
+                // Install pytest first, then run it
+                steps.sh '''
+                python3 -m pip install --user pytest
+                python3 -m pytest
+                '''
+                break
+            case 'go':
+                steps.sh 'go test ./...'
+                break
+            case 'java':
+                steps.sh 'mvn test'
+                break
+            case 'node':
+                steps.sh 'npm test'
+                break
         }
     }
 }
