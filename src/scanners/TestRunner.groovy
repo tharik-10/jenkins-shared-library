86c7eda7797b17cdd819a13a99060493c478064a
tharik-10 <md.tharik@mygurukulam.co>
2026-02-03 12:45:44 +0530

    Create TestRunner.groovy

---
+++ src/scanners/TestRunner.groovy
@@ -0,0 +1,20 @@
+package scanners
+
+class TestRunner {
+    static void run(String lang) {
+        switch(lang) {
+            case 'python':
+                sh 'pytest'
+                break
+            case 'go':
+                sh 'go test ./...'
+                break
+            case 'java':
+                sh 'mvn test'
+                break
+            case 'node':
+                sh 'npm test -- --watch=false'
+                break
+        }
+    }
+}
