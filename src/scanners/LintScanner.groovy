3d7d8bcd11c924331e0cfee2efda4e69a9036358
tharik-10 <md.tharik@mygurukulam.co>
2026-02-03 12:45:14 +0530

    Create LintScanner.groovy

---
+++ src/scanners/LintScanner.groovy
@@ -0,0 +1,22 @@
+package scanners
+
+class LintScanner {
+    static void run(String lang) {
+        switch(lang) {
+            case 'python':
+                sh 'pip install flake8 && flake8 .'
+                break
+            case 'go':
+                sh 'golangci-lint run'
+                break
+            case 'java':
+                sh 'mvn checkstyle:check'
+                break
+            case 'node':
+                sh 'npm install && npm run lint'
+                break
+            default:
+                error "Unsupported language"
+        }
+    }
+}
