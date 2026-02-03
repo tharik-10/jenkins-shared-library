afd3516900a246b39bb1ca5edae2474c25539bf8
tharik-10 <md.tharik@mygurukulam.co>
2026-02-04 04:58:03 +0530

    Update LintScanner.groovy

--- src/scanners/LintScanner.groovy
+++ src/scanners/LintScanner.groovy
@@ -1,16 +1,11 @@
 package scanners
 
 class LintScanner {
-    // 1. Remove 'static' (it's cleaner to pass context to a constructor or method)
-    // 2. Add 'steps' as a parameter
     static void run(def steps, String lang) {
+        steps.echo "Running Linting for ${lang}..."
         switch(lang) {
             case 'python':
-                // Use 'steps.sh' instead of just 'sh'
-                steps.sh '''
-                python3 -m pip install --user flake8
-                /var/lib/jenkins/.local/bin/flake8 . || python3 -m flake8 .
-                '''
+                steps.sh 'python3 -m pip install --user flake8 && python3 -m flake8 .'
                 break
             case 'go':
                 steps.sh 'golangci-lint run'
