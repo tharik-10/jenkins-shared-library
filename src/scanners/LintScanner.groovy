939d0c094fa3588e90f574e0c3562f437df17ad9
tharik-10 <md.tharik@mygurukulam.co>
2026-02-03 18:32:38 +0530

    Update LintScanner.groovy

--- src/scanners/LintScanner.groovy
+++ src/scanners/LintScanner.groovy
@@ -4,9 +4,11 @@ class LintScanner {
     static void run(def steps, String lang) {
         switch(lang) {
             case 'python':
-                steps.sh '''
-                pip install flake8
-                flake8 .
+                // Using 'python3 -m' ensures we use the module we just installed
+                // even if the standalone 'flake8' command isn't in the PATH
+                sh '''
+                python3 -m pip install --user flake8
+                python3 -m flake8 .
                 '''
                 break
             case 'go':
