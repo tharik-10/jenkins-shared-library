af8d51d558de02aa4aaea330428228df65319be2
tharik-10 <md.tharik@mygurukulam.co>
2026-02-03 18:36:46 +0530

    Update LintScanner.groovy

--- src/scanners/LintScanner.groovy
+++ src/scanners/LintScanner.groovy
@@ -1,14 +1,15 @@
 package scanners
 
 class LintScanner {
+    // 1. Remove 'static' (it's cleaner to pass context to a constructor or method)
+    // 2. Add 'steps' as a parameter
     static void run(def steps, String lang) {
         switch(lang) {
             case 'python':
-                // Using 'python3 -m' ensures we use the module we just installed
-                // even if the standalone 'flake8' command isn't in the PATH
-                sh '''
+                // Use 'steps.sh' instead of just 'sh'
+                steps.sh '''
                 python3 -m pip install --user flake8
-                python3 -m flake8 .
+                /var/lib/jenkins/.local/bin/flake8 . || python3 -m flake8 .
                 '''
                 break
             case 'go':
@@ -18,11 +19,10 @@ class LintScanner {
                 steps.sh 'mvn checkstyle:check'
                 break
             case 'node':
-                steps.sh '''
-                npm ci
-                npm run lint
-                '''
+                steps.sh 'npm run lint'
                 break
+            default:
+                steps.error "Unsupported language: ${lang}"
         }
     }
 }
