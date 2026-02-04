bf68f954c07c633e01693e64ef61ca4caeb4e916
tharik-10 <md.tharik@mygurukulam.co>
2026-02-04 11:52:18 +0530

    Update DockerBuild.groovy

--- src/docker/DockerBuild.groovy
+++ src/docker/DockerBuild.groovy
@@ -1,12 +1,27 @@
 package docker
 
 class DockerBuild {
-    static void buildAndPush(def steps, String ecr, String app, String tag) {
-        steps.echo "Building and Pushing Docker image to ECR..."
+    static void buildAndPush(def steps, String ecrUrl, String appName, String tag) {
+        String region = "ap-south-1"
+        
+        steps.echo "Checking if ECR repository ${appName} exists..."
+        
+        // This command checks for the repo; if it fails, it creates it.
         steps.sh """
-        docker build -t ${app}:${tag} .
-        docker tag ${app}:${tag} ${ecr}/${app}:${tag}
-        docker push ${ecr}/${app}:${tag}
+            aws ecr describe-repositories --repository-names ${appName} --region ${region} || \
+            aws ecr create-repository --repository-name ${appName} --region ${region}
+        """
+
+        steps.echo "Logging into Amazon ECR..."
+        steps.sh "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ecrUrl}"
+
+        steps.echo "Building and Pushing Docker image..."
+        String fullImageName = "${ecrUrl}/${appName}:${tag}"
+        
+        steps.sh """
+            docker build -t ${appName}:${tag} .
+            docker tag ${appName}:${tag} ${fullImageName}
+            docker push ${fullImageName}
         """
     }
 }
