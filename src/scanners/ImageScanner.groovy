a26728917636217e83e8b45cb0cb3cd3b5519074
tharik-10 <md.tharik@mygurukulam.co>
2026-02-04 12:35:22 +0530

    Update ImageScanner.groovy

--- src/scanners/ImageScanner.groovy
+++ src/scanners/ImageScanner.groovy
@@ -3,10 +3,15 @@ package scanners
 class ImageScanner {
     static void scan(def steps, String ecr, String app, String tag) {
         steps.echo "Scanning Docker Image: ${app}:${tag}"
+        
+        // Added --ignore-unfixed to bypass vulnerabilities that have no fix yet
+        // and --no-progress to keep the Jenkins logs clean.
         steps.sh """
-        trivy image ${ecr}/${app}:${tag} \
-        --severity HIGH,CRITICAL \
-        --exit-code 1
+            trivy image ${ecr}/${app}:${tag} \
+            --severity HIGH,CRITICAL \
+            --ignore-unfixed \
+            --no-progress \
+            --exit-code 1
         """
     }
 }
